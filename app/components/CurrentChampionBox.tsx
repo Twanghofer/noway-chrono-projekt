86a3507e12fdc3a6fa0b8b4d7b179b9efa414ffc
twanghofer <tobias.wanghofer@gmail.com>
2024-12-01 22:20:40 +0100

    Add champion name and release date to CurrentChampionBox

--- app/components/CurrentChampionBox.tsx
+++ app/components/CurrentChampionBox.tsx
@@ -28,53 +28,66 @@ export default function CurrentChampionBox(
   }, [props.champion]);
 
   return (
-    <div className="flex flex-row gap-4 flex-wrap">
-      <div>
-        <ChampionAvatar champion={props.champion} className="self-start" />
-        <div className="text-center text-lg font-medium">
-          {currentChampionStats?.wins} - {currentChampionStats?.losses}
-        </div>
+    <div>
+      <div className="mb-2">
+        <h3 className="text-4xl font-medium">{props.champion.name}</h3>
+
+        <p className="opacity-50 font-light">
+          Release date:{" "}
+          {format.dateTime(new Date(props.champion.releaseDate), {
+            dateStyle: "medium",
+          })}
+        </p>
       </div>
 
-      {props.champion.matches && (
-        <ul className="space-y-3 flex-grow md:flex-grow-0 md:w-fit min-w-80 max-w-full">
-          {props.champion.matches.slice(0, 3).map((match) => (
-            <li
-              key={match.id}
-              className={`min-h-24 p-3 rounded-lg ${match.win ? "bg-green-900" : "bg-red-900"}`}
-            >
-              <div className="flex gap-5 items-center justify-between">
-                <div className="space-y-1.5">
-                  <ChampionAvatar
-                    champion={props.champion}
-                    className="w-8 h-8 rounded-full size-14"
-                  />
-                  <div className="capitalize text-xs opacity-75 self-start">
-                    {format.relativeTime(match.timestamp)}
+      <div className="flex flex-row gap-4 flex-wrap">
+        <div>
+          <ChampionAvatar champion={props.champion} className="self-start" />
+          <div className="text-center text-lg font-medium">
+            {currentChampionStats?.wins} - {currentChampionStats?.losses}
+          </div>
+        </div>
+
+        {props.champion.matches && (
+          <ul className="space-y-3 flex-grow md:flex-grow-0 md:w-fit min-w-80 max-w-full">
+            {props.champion.matches.slice(0, 3).map((match) => (
+              <li
+                key={match.id}
+                className={`min-h-24 p-3 rounded-lg ${match.win ? "bg-green-900" : "bg-red-900"}`}
+              >
+                <div className="flex gap-5 items-center justify-between">
+                  <div className="space-y-1.5">
+                    <ChampionAvatar
+                      champion={props.champion}
+                      className="w-8 h-8 rounded-full size-14"
+                    />
+                    <div className="capitalize text-xs opacity-75 self-start">
+                      {format.relativeTime(match.timestamp)}
+                    </div>
                   </div>
-                </div>
 
-                <div>
-                  {match.playerStats.kills} /{" "}
-                  <span className="font-medium text-red-400">
-                    {match.playerStats.deaths}
-                  </span>{" "}
-                  / {match.playerStats.assists}
-                </div>
+                  <div>
+                    {match.playerStats.kills} /{" "}
+                    <span className="font-medium text-red-400">
+                      {match.playerStats.deaths}
+                    </span>{" "}
+                    / {match.playerStats.assists}
+                  </div>
 
-                <Button className="ml-auto" asChild>
-                  <Link
-                    href={`https://www.deeplol.gg/summoner/euw/MatheMann4u-EUW/matches/${match.id}`}
-                    target="_blank"
-                  >
-                    Details
-                  </Link>
-                </Button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+                  <Button className="ml-auto" asChild>
+                    <Link
+                      href={`https://www.deeplol.gg/summoner/euw/MatheMann4u-EUW/matches/${match.id}`}
+                      target="_blank"
+                    >
+                      Details
+                    </Link>
+                  </Button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
