4607987782898ee4bc8dd4f4be39505d852530ab
twanghofer <tobias.wanghofer@gmail.com>
2024-12-06 14:07:21 +0100

    Set match update cronjob interval to 15min

--- convex/crons.ts
+++ convex/crons.ts
@@ -3,6 +3,6 @@ import { api } from "./_generated/api";
 
 const crons = cronJobs();
 
-crons.interval("update-matches", { minutes: 30 }, api.matches.update);
+crons.interval("update-matches", { minutes: 15 }, api.matches.update);
 
 export default crons;
